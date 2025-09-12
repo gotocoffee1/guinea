8f2764902a6c83aaf67e1561b51fcdc7b0f91aca
gotocoffee <gotocoffee1@gmail.com>
2025-09-12 14:19:07 +0200

    bump imgui

--- include/scoped/ui.hpp
+++ include/scoped/ui.hpp
@@ -39,13 +39,13 @@ struct Child
 {
     bool IsContentVisible;
 
-    Child(const char* str_id, const ImVec2& size = ImVec2(0, 0), bool border = false, ImGuiWindowFlags flags = 0)
+    Child(const char* str_id, const ImVec2& size = ImVec2(0, 0), ImGuiChildFlags child_flags = 0, ImGuiWindowFlags flags = 0)
     {
-        IsContentVisible = ImGui::BeginChild(str_id, size, border, flags);
+        IsContentVisible = ImGui::BeginChild(str_id, size, child_flags, flags);
     }
-    Child(ImGuiID id, const ImVec2& size = ImVec2(0, 0), bool border = false, ImGuiWindowFlags flags = 0)
+    Child(ImGuiID id, const ImVec2& size = ImVec2(0, 0), ImGuiChildFlags child_flags = 0, ImGuiWindowFlags flags = 0)
     {
-        IsContentVisible = ImGui::BeginChild(id, size, border, flags);
+        IsContentVisible = ImGui::BeginChild(id, size, child_flags, flags);
     }
     ~Child()
     {
